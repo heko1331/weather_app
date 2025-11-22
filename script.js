95ddf449dc39e6f998b100d8d89f0a825849134c
artur <grishunin_artur@mail.ru>
2025-11-22 22:20:20 +0300

    js variables

---
+++ script.js
@@ -0,0 +1,13 @@
+const key = '36da10c2410c9d4f8e409c462e441f1c';
+const input = document.querySelector('input');
+const karinka_temp = document.querySelector('#karinka_temp');
+const pogoda_date = document.querySelector('.date');
+const weather_description = document.querySelector('#weather_description');
+const min_temp = document.querySelector('#min_temp');
+const max_temp = document.querySelector('#max_temp');
+const ochuchenye = document.querySelector('#ochuchenye');
+
+// input.addEventListener('keypress', a);
+// function a(event){
+//     console.log(event);
+// }
